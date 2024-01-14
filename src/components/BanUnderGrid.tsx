fc493e0ea3495422c58e2f278c92c21ca019e60a
Muhammad Umar Chaudhry <ch256.it@gmail.com>
2024-01-14 14:27:17 +0500

    BanUnderGrid Completed

--- src/components/BanUnderGrid.tsx
+++ src/components/BanUnderGrid.tsx
@@ -2,9 +2,24 @@ import BanUnderGridImg from "../assets/svg/banner1.svg";
 
 function BanUnderGrid() {
   return (
-    <div className="relative px-3 md:px-12 py-[100px]">
-      <div className="border-2 border-green-600 rounded-xl bg-newGreen">
+    <div className="px-3 md:px-12 py-[100px]">
+      <div className="relative rounded-xl overflow-hidden border border-BanUndGrdGreen">
         <img className="w-[100%]" src={BanUnderGridImg} />
+        <div className="absolute h-full w-full top-0 left-0 bottom-0 right-0 bg-BanUndGrdGreen "></div>
+        <div className="absolute right-0 left-0 top-0 bottom-0 m-auto flex flex-col items-center justify-center gap-5">
+          <p className="max-w-[770px] text-white text-4xl leading-[46px] font-semibold text-center">
+            Unlock the Power of Your Ideas — Request Your Free Project Quote
+            Now.
+          </p>
+          <p className="max-w-[580px] text-white leading-5 text-center">
+            Our team of experts is dedicated to understanding your unique needs,
+            offering tailored solutions, and ensuring that every detail aligns
+            with your goals.
+          </p>
+          <button className="px-10 py-[10px] bg-white text-newGreen rounded-full font-semibold md:hover:text-white md:hover:bg-transparent border-2 border-white">
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
   );
